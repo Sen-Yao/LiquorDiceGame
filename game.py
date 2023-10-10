a8de16bdc307f50ed6ad78e2952eaf9d4abebc32
Sen-Yao <1574653569@qq.com>
2023-10-10 16:16:56 +0800

    建立基本框架

---
+++ game.py
@@ -0,0 +1,10 @@
+class LiquorDiceGame:
+
+    def __init__(self, zhai_rule, jump_open_rule):
+        """
+        Initialize the class LiquorDiceGame
+        """
+        self.zhai_rule = zhai_rule
+        self.jump_open_rule = jump_open_rule
+
+    pass
