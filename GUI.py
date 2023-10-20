6b771f116fa726d41e53999ab0d3d04e1c06424b
“xiazhi1” <2194625680@qq.com>
2023-10-20 17:25:44 +0800

    add some IO function

--- GUI.py
+++ GUI.py
@@ -1,20 +1,24 @@
-import sys
+import sys,re
 from PyQt5.QtWidgets import (QLabel, QDialog, QPushButton, QApplication, QLineEdit, QWidget)
-from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout, QCheckBox)
+from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout, QCheckBox, QMessageBox)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 
 
-class MainWindow(QWidget):
+class GUI(QWidget):
 
     def __init__(self):
         super().__init__()
         self.user_input=None # 储存用户的输入
-        self.is_open=False # 判断是否开
-        self.is_jump_open=False # 判断是否跳开
+        self.is_open = False # 判断是否开
+        self.is_jump_open = False # 判断是否跳开
+        self.is_guess = False # 判断是否继续猜测
         self.is_use_one=False # 判断是否喊一
         self.player_dice=[1,2,3,4,5] # 玩家骰子结果，测试用，实际从服务器读取
         self.current_player=1 # 玩家游戏序号，测试用，实际从服务器读取
+        self.last_guess= [7,2,1] # 上一游戏玩家的猜测
+        # 该变量储存实际上当前各玩家手上骰子的总和结果 依次代表有多少个1，2，3，4，5，6
+        self.results=[6,6,6,6,6,6] 
         self.initWindow()
 
     # 初始化主窗口           
@@ -25,7 +29,7 @@ class MainWindow(QWidget):
         jumpButton = QPushButton("跳开")
         label = QLabel(
             "欢迎使用LiquorDiceGame！\n您是" + str(self.current_player) + "号玩家" "\n您的骰子结果为" + ','.join(
-                map(str, self.player_dice)) + "\n请点击下方按钮做出您下一步的操作", self)
+                map(str, self.player_dice)) +"\n上一玩家猜测结果为" + ','.join(map(str, self.last_guess))+ "\n请点击下方按钮做出您下一步的操作", self)
 
         # 对主界面进行盒布局
         hbox = QHBoxLayout()
@@ -51,6 +55,7 @@ class MainWindow(QWidget):
 
     # 该函数用于创建继续猜测的对话对象和读入用户输入
     def create_dialog(self):
+        self.is_guess = True
         dialog = Dialog()
         result = dialog.exec_()  # 显示对话窗口并等待用户交互
 
@@ -65,12 +70,37 @@ class MainWindow(QWidget):
     # 该函数用于处理开
     def open_action(self):
         self.is_open = True
-        # print(self.is_open)
-
+        self.show_results()
+        
     # 该函数用于处理跳开
     def jump_open_action(self):
         self.is_jump_open = True
-        # print(self.is_jump_open)
+        self.show_results()
+
+    # 该函数用于展示结果
+    def show_results(self):
+        # 创建展示窗口
+        show_window = QDialog()
+
+        # 展示结果标签
+        results_label = QLabel(self)
+        results_text = '最后各玩家的总结果为 '
+        for index, nums in enumerate(self.results):
+            results_text += f'{nums}个{index+1} '
+        results_label.setText(results_text)
+        
+        # 对子窗口进行盒布局
+        hbox = QHBoxLayout()
+        hbox.addWidget(results_label)
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+        show_window.setLayout(vbox)
+
+        # 设计窗口并显示
+        show_window.setWindowTitle('结果展示')
+        show_window.setGeometry(200, 200, 300, 200)
+        show_window.show()
+        show_window.exec_()
 
 
 class Dialog(QDialog):
@@ -79,11 +109,10 @@ class Dialog(QDialog):
 
         self.initDialog()
         self.user_input = None  # 初始化一个属性来保存用户输入
-        self.is_use_one = False # 初始化一个布尔型变量保存是否加一
+        self.is_use_one = False # 初始化一个布尔型变量保存是否喊一
     
     # 初始化对话栏 
     def initDialog(self):
-
         self.setWindowTitle('继续猜测')
         self.setGeometry(200, 200, 300, 200)
         layout = QVBoxLayout()
@@ -109,8 +138,15 @@ class Dialog(QDialog):
 
         self.setLayout(layout)
 
-    # 返回用户输入
+    # 判断并返回用户输入
     def get_user_input(self):
+        user_input = self.text_input.text()
+        # 使用正则表达式验证输入
+        input_pattern = r'^\d+\s\d+\s\d+$'  # 匹配三个带空格的数字
+        if not re.match(input_pattern, user_input):
+            QMessageBox.warning(self, '错误', '输入不符合规范，请重新输入（例如：7 4 2）', QMessageBox.Ok)
+            self.text_input.clear()  # 清空文本框
+
         return self.text_input.text()
     
     #返回用户勾选
@@ -121,8 +157,7 @@ class Dialog(QDialog):
             self.is_use_one=False
         return self.is_use_one
 
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MainWindow()
+    ex = GUI()
     sys.exit(app.exec_())
