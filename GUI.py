9d824aaa437538831964ee306478a5498bcbfd90
xiazhi1 <2194625680@qq.com>
2023-11-01 00:44:31 +0800

    a test way to fix GUI

--- GUI.py
+++ GUI.py
@@ -4,103 +4,105 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import (QLabel, QDialog, QPushButton, QApplication, QLineEdit, QWidget)
 from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout, QCheckBox,QMessageBox)
 from PyQt5.QtGui import QIcon,QIntValidator
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,QObject, pyqtSignal
 from web import libclient
 import json
 
 
 class GUI(QWidget):
 
-    def __init__(self,read_server_fn):
+    def __init__(self):
         super().__init__()
+        self.type = None
         
-        self.read_server_fn = read_server_fn 
-        self.type= read_server_fn['type']
-
-
-        # 当传入的字典type为start_mesg
-        if self.type == 'start_mesg':
-            self.current_round = read_server_fn['current_round']
-            self.dice = read_server_fn['dice']
-            self.player_id = read_server_fn['player_id']
-            self.player_name = read_server_fn['player_name']
+        # start_mesg type
+        self.current_round = None
+        self.dice = None
+        self.player_id = None
+        self.player_name = None
         # 当传入的字典type为ask
-        elif self.type == 'Ask':
-            self.is_your_round = 1
-            self.num = None
-            self.face = None
-            self.zhai = None    # 分别储存用户具体猜测
-            # 储存上一个用户的猜测
-            self.last_num = read_server_fn['last_guess_num']
-            self.last_face = read_server_fn['last_guess_face']
-            self.last_zhai = read_server_fn['last_guess_zhai']
+        
+        self.is_your_round = None
+        self.num = None
+        self.face = None
+        self.zhai = None    # 分别储存用户具体猜测
+        # 储存上一个用户的猜测
+        self.last_num = None
+        self.last_face = None
+        self.last_zhai = None
 
-            self.user_input = None  # 储存用户的猜测输入
-            self.is_open = None  # 判断是否开
-            self.is_jump_open = None  # 判断是否跳开
-            self.is_guess = None  # 判断是否进行猜测
-            self.is_use_one = None  # 判断是否喊一 
+        self.user_input = None  # 储存用户的猜测输入
+        self.is_open = None  # 判断是否开
+        self.is_jump_open = None  # 判断是否跳开
+        self.is_guess = None  # 判断是否进行猜测
+        self.is_use_one = None  # 判断是否喊一 
               
-        # 当传入的字典type为s2c_decide
-        elif self.type == 's2c_decide':
-            self.player_id = read_server_fn['player_id'] # 广播用户id
-            self.player_name = read_server_fn['player_name'] # 广播用户name
-            self.num = read_server_fn['num'] # 广播用户猜测
-            self.face = read_server_fn['face']
-            self.zhai = read_server_fn['zhai']
-            
+
         # 当传入的字典type为end
-        elif self.type == 'end':
-            self.dices = read_server_fn['dice'] # 最终结果
-            self.names = read_server_fn['name'] # 最终玩家姓名
-            self.info = read_server_fn['info'] 
+        
+        self.dices = None # 最终结果
+        self.names = None # 最终玩家姓名
+        self.info = None
 
         self.initWindow()
 
     # 初始化主窗口           
     def initWindow(self):
-
+        if self.layout() is not None:
+            # 删除布局
+            old_layout = self.layout()
+            while old_layout.count():
+                old_layout_item = old_layout.takeAt(0)
+                if old_layout_item.widget():
+                    old_layout_item.widget().deleteLater()
+        if self.layout() is not None:
+            print('wrong')
+        self.name_welcome_label = QLabel(str(self.player_name)+"，您好！\n")
+        # start_mesg 
         if self.type == 'start_mesg':
-            name_welcome_label = QLabel(str(self.player_name)+"，您好！\n")
-            
-            welcome_label = QLabel(
+            self.welcome_label = QLabel(
             "欢迎使用LiquorDiceGame！\n,当前游戏轮次为第"+str(self.current_round)+",您是" + str(self.player_id) + "号玩家" "\n您的骰子结果为" + ','.join(
                 map(str, self.dice))+"\n当前未到您的轮次，请您耐心等待!" , self)
-        elif self.type == 's2c_decide':
-            broadcast_label = QLabel("现在"+str(self.player_id)+"号玩家"+str(self.player_name)+"的猜测结果是"+str(self.num)+str(self.face)+str(self.zhai)+"请您参考并等待进行下一步操作")
-        elif self.type == 'end':
-            end_label = QLabel("游戏结束，各玩家的结果为"+str(self.dices)+"\n参与本轮游戏的玩家为"+str(self.names))
-            end_info = QLabel(self.info)
-        # 创建按钮供用户选择两种游戏策略
-        openButton = QPushButton("开")
-        continueButton = QPushButton("继续猜测")
-        action_label=QLabel("\n当前轮到您的轮次，请从下面两个按钮中选择您要进行的操作")
+            self.welcome_label.setVisible(False)
+        # broadcast_mesg
+        if self.type == 's2c_decide':
+            self.broadcast_label = QLabel("现在"+str(self.player_id)+"号玩家"+str(self.player_name)+"的猜测结果是"+str(self.num)+str(self.face)+str(self.zhai)+"请您参考并等待进行下一步操作")
+            self.broadcast_label.setVisible(False)
+        # end_mesg
+        if self.type == 'end':
+            self.end_label = QLabel("游戏结束，各玩家的结果为"+str(self.dices)+"\n参与本轮游戏的玩家为"+str(self.names))
+            self.end_info = QLabel(self.info)
+            self.end_label.setVisible(False)
+            self.end_info.setVisible(False)
+        # 创建按钮供用户选择两种游戏策略（ask）
+        self.openButton = QPushButton("开")
+        self.continueButton = QPushButton("继续猜测")
+        self.action_label=QLabel("\n当前轮到您的轮次，请从下面两个按钮中选择您要进行的操作")
+        self.openButton.setVisible(False)
+        self.continueButton.setVisible(False)
+        self.action_label.setVisible(False)
+        
         
         # 对主界面进行盒布局
         hbox = QHBoxLayout()
-        if self.type == 'Ask':
-            hbox.addWidget(openButton)
-            hbox.addWidget(continueButton)
-            # 根据按钮的点击情况选择不同的函数处理
-            continueButton.clicked.connect(self.create_dialog)
-            openButton.clicked.connect(self.open_action)
+        hbox.addWidget(self.openButton)
+        hbox.addWidget(self.continueButton)
+        # 根据按钮的点击情况选择不同的函数处理
+        self.continueButton.clicked.connect(self.create_dialog)
+        self.openButton.clicked.connect(self.open_action)
         vbox = QVBoxLayout()
-
-        
-        if self.type == 'start_mesg' :
-            vbox.addWidget(welcome_label)
-        elif self.type == 's2c_decide':
-            vbox.addWidget(broadcast_label)
-        elif self.type == 'end':
-            vbox.addWidget(end_label)
-            vbox.addWidget(end_info)
-        elif self.type == 'Ask':
-            vbox.addWidget(action_label)
-
+        vbox.addWidget(self.name_welcome_label)
+        vbox.addWidget(self.action_label)
         if self.type == 'start_mesg':
-            vbox.addWidget(name_welcome_label)
+            vbox.addWidget(self.welcome_label)
+        if self.type == 's2c_decide':
+            vbox.addWidget(self.broadcast_label)
+        if self.type == 'end':
+            vbox.addWidget(self.end_label)
+            vbox.addWidget(self.end_info)
         vbox.addLayout(hbox)
-        self.setLayout(vbox)
+        if self.type is None:
+            self.setLayout(vbox)
         # 设置窗口风格 大小图标
         self.setGeometry(300, 300, 350, 250)
         self.setWindowTitle('LiquorDiceGame')
@@ -117,7 +119,62 @@ class GUI(QWidget):
             self.num,self.face,self.zhai = dialog.get_user_input()
             self.is_use_one=dialog.verify_use_one()
 
-        
+    def update_GUI(self,read_server_fn,read_server,write_server):
+
+        while True:
+            if read_server_fn['type'] == 'start_mesg':
+                self.type = 'start_mesg'
+                self.welcome_label.setVisible(True)
+                self.current_round = read_server_fn['current_round']
+                self.dice = read_server_fn['dice']
+                self.player_id = read_server_fn['player_id']
+                self.player_name = read_server_fn['player_name']
+            elif read_server_fn['type'] == 'Ask':
+                self.type = 'Ask'
+                self.openButton.setVisible(True)
+                self.continueButton.setVisible(True)
+                self.action_label.setVisible(True)
+                # set others widget false
+                # self.welcome_label.setVisible(False)
+                # 储存上一个用户的猜测
+                self.last_num = read_server_fn['last_guess_num']
+                self.last_face = read_server_fn['last_guess_face']
+                self.last_zhai = read_server_fn['last_guess_zhai']
+                # 玩家继续猜测的输入，用于返回到服务器端
+                if read_server_fn['type'] == 'Ask':
+                    decide = {
+                        'type': 'decide',
+                        'num': self.num,  # 若为0则代表玩家选择开
+                        'face': self.face,
+                        'zhai': self.zhai
+                    }
+                    decide_mesg_json = json.dumps(decide)
+                    write_server(decide_mesg_json)
+            elif read_server_fn['type'] == 's2c_decide':
+                self.type = 's2c_decide'
+                self.broadcast_label.setVisible(True)
+                self.player_id = read_server_fn['player_id'] # 广播用户id
+                self.player_name = read_server_fn['player_name'] # 广播用户name
+                self.num = read_server_fn['num'] # 广播用户猜测
+                self.face = read_server_fn['face']
+                self.zhai = read_server_fn['zhai']
+            elif read_server_fn['type'] == 'end':
+                self.type = 'end'
+                self.dices = read_server_fn['dice'] # 最终结果
+                self.names = read_server_fn['name'] # 最终玩家姓名
+                self.info = read_server_fn['info'] 
+            self.initWindow()
+            next_read_server_str = read_server()
+            print(next_read_server_str)
+            # # convert json's type to dict
+            next_read_server_fn = json.loads(next_read_server_str)
+            if next_read_server_fn is not None:
+                self.update_GUI(read_server_fn=next_read_server_fn,read_server=read_server,write_server=write_server)
+
+                
+            
+            
+
     # 该函数用于处理开
     def open_action(self):
         self.is_open = True
@@ -288,51 +345,25 @@ def create_name():
         # print(f'用户输入的文本是: {self.player_name}')
         return player_name
 
+
 def main_client():
-
-    # # test code
-    # read_server_fn =   {
-    #     'type': 'Ask',
-    #     'is_your_round': 1,
-    #     'last_guess_num': 7,
-    #     'last_guess_face':4,
-    #     'last_guess_zhai':1
-
-    # }
-    # 用于判断是否需要建立连接
-    begin_connect = True
+    app = QApplication(sys.argv) 
+    # create name to client
+    read_server,write_server = libclient.get_remote_fn(server_ip='127.0.0.1', server_port=12347)
+    client=GUI()
+    player_name = create_name()
+    write_server(player_name)
+    # read from server
+    success_connect_str = read_server()
+    print(success_connect_str)
+    read_server_str = read_server()
+    print(read_server_str)
+    # # convert json's type to dict
+    read_server_fn = json.loads(read_server_str)
     
-    while True:
-
-        app = QApplication(sys.argv)
-        # create name to client
-        read_server,write_server = libclient.get_remote_fn(server_ip='127.0.0.1', server_port=12347)
-        if begin_connect is True:
-            player_name = create_name()
-            write_server(player_name)
-            begin_connect = False
-        # read frpm server
-        success_connect_str = read_server()
-        print(success_connect_str)
-        read_server_str = read_server()
-        print(read_server_str)
-        # # convert json's type to dict
-        read_server_fn = json.loads(read_server_str)
-        
-        client=GUI(read_server_fn=read_server_fn)
-        
-        # 玩家继续猜测的输入，用于返回到服务器端
-        if read_server_fn['type'] == 'Ask':
-            decide = {
-                'type': 'decide',
-                'num': client.num,  # 若为0则代表玩家选择开
-                'face': client.face,
-                'zhai': client.zhai
-            }
-            decide_mesg_json = json.dumps(decide)
-            write_server(decide_mesg_json)
-
-        sys.exit(app.exec_())
+    if read_server_fn is not None:
+        client.update_GUI(read_server_fn=read_server_fn,read_server=read_server,write_server=write_server)
+    sys.exit(app.exec_())
 
 
 
